dede2fa5b46c01ba3cac23be2490ba3b81255076
Hillson <hillsonshalom@gmail.com>
2024-08-21 16:42:30 +0300

    Creating a DbContext file

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using FinalProjectAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProjectAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<Target> Targets { get; set; }
+        public DbSet<Mission> Missions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Mission>()
+                .HasOne(e => e.Agent)
+                .WithMany()
+                .HasForeignKey(e => e.AgentId);
+
+            modelBuilder.Entity<Mission>()
+                .HasOne(e => e.Target)
+                .WithMany()
+                .HasForeignKey(e => e.TargetId);
+
+            modelBuilder.Entity<Mission>()
+                .HasKey(m => new { m.AgentId, m.TargetId });
+        }
+    }
+}
