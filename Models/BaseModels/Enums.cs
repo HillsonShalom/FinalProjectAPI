5f1dc468de6f14a3fc4e5322e38356dd566cf2d7
Hillson <hillsonshalom@gmail.com>
2024-08-21 15:46:48 +0300

    יצרתי מודלים בסיסיים כמו מחלקה אבסטרקטית ל"ישות במרחב" ואינמים

---
+++ Models/BaseModels/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace FinalProjectAPI.Models.BaseModels
+{
+    public enum AgentStatus
+    {
+        Dormant,
+        Active
+    }
+
+    public enum TargetStatus
+    {
+        Living,
+        Eliminated
+    }
+
+    public enum MissionStatus
+    {
+        Offerd,
+        Active,
+        Done
+    }
+}
