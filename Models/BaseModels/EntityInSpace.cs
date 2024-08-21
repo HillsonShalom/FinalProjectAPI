02a41024d43a43d7eb4a38cab9a8601496ed1a8b
Hillson <hillsonshalom@gmail.com>
2024-08-21 16:46:26 +0300

    I've added an atribute that makes one geter property out of DB

--- Models/BaseModels/EntityInSpace.cs
+++ Models/BaseModels/EntityInSpace.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace FinalProjectAPI.Models.BaseModels
 {
@@ -10,7 +11,7 @@ namespace FinalProjectAPI.Models.BaseModels
         public int LocationX { get; set; }
         public int LocationY { get; set; }
 
-
+        [NotMapped]
         public Point Location
         {
             get
